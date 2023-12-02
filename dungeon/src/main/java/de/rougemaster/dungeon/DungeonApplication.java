e90e6ebba578fbae06fc0ec8e1d5d388e399f52b
TimeSpirit8 <107579242+TimeSpirit8@users.noreply.github.com>
2023-12-02 16:32:00 +0100

    Project Setup

---
+++ dungeon/src/main/java/de/rougemaster/dungeon/DungeonApplication.java
@@ -0,0 +1,13 @@
+package de.rougemaster.dungeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DungeonApplication.class, args);
+	}
+
+}
---
+++ player/src/main/java/de/roguemaster/player/PlayerApplication.java
@@ -0,0 +1,13 @@
+package de.roguemaster.player;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerApplication.class, args);
+	}
+
+}
