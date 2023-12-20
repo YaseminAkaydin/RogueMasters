e070af09d2505c18b914f22b4a96f4eb13967c0a
acx806 <yasemin.akaydin@haw-hamburg.de>
2023-12-20 14:59:58 +0100

    Item Modul hinzugefügt

---
+++ item/src/main/java/de/roguemasters/item/Item.java
@@ -0,0 +1,14 @@
+package de.roguemasters.item;
+
+public abstract class Item {
+    private int id;
+    private String name;
+    private String description;
+
+    // Konstruktor für die Klasse Item
+    public Item(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+}
---
+++ item/src/main/java/de/roguemasters/item/Weapon.java
@@ -0,0 +1,17 @@
+package de.roguemasters.item;
+
+public class Weapon extends Eqipable{
+    private int damage;
+    public Weapon(int id, String name, String description, int damage) {
+        super(id, name, description);
+        this.damage=damage;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+}
