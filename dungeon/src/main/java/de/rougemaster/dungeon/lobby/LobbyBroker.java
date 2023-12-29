26af42e9a25980dc8c930a630c89116b6629e40f
TimeSpirit8 <107579242+TimeSpirit8@users.noreply.github.com>
2023-12-30 00:59:19 +0100

    created Base GameFiles

---
+++ dungeon/src/main/java/de/rougemaster/dungeon/lobby/LobbyBroker.java
@@ -0,0 +1,7 @@
+package de.rougemaster.dungeon.lobby;
+
+public class LobbyBroker {
+    public static class LobbyBrokerRegister {
+
+    }
+}
---
+++ dungeon/src/main/java/de/rougemaster/dungeon/lobby/LobbyFacade.java
@@ -0,0 +1,4 @@
+package de.rougemaster.dungeon.lobby;
+
+public class LobbyFacade {
+}
