927a6402a9cb8f4d7248cd47c95e7b342ebc7120
hosso <hovsep.aroyan@haw-hamburg.de>
2024-01-03 06:00:30 +0100

    Kleiner fix --> UML

--- player/src/main/java/de/roguemaster/player/CLIneu/InventoryAction.java
+++
@@ -1,8 +0,0 @@
-package de.roguemaster.player.CLIneu;
-
-public enum InventoryAction {
-    CHOOSE_ACTION, // Initial state where the player chooses Drop, Use, etc.
-    DROP_ITEM,
-    USE_CONSUMABLE,
-    VIEWING // Viewing the inventory without making a choice
-}
