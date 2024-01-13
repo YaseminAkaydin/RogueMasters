d9e00f033edc93206ede9319b3630087ac4ad5e7
Maurice <maurice.duemmel@haw-hamburg.de>
2024-01-13 19:00:14 +0100

    implemented fight action handling in TurnManager

--- dungeon/src/main/java/de/rougemaster/dungeon/game/TurnManager.java
+++ dungeon/src/main/java/de/rougemaster/dungeon/game/TurnManager.java
@@ -3,9 +3,22 @@ package de.rougemaster.dungeon.game;
 import de.rougemaster.dungeon.character.Character;
 import de.rougemaster.dungeon.character.playerCharacter.PlayableCharacter;
 import de.rougemaster.dungeon.dungeon.Room;
+import de.rougemaster.dungeon.game.fight.CombatAction;
+import de.rougemaster.dungeon.game.fight.Fight;
 import de.rougemaster.dungeon.game.fight.FightManager;
 import de.rougemaster.dungeon.game.gameCommand.GameCommand;
 import de.rougemaster.dungeon.game.gameCommand.CharacterCommands.doNothingGameCommand;
+import de.rougemaster.dungeon.game.gameCommand.devilCommands.devilFlameSwordAttackCommand;
+import de.rougemaster.dungeon.game.gameCommand.devilCommands.devilPlayerKillerAttackCommand;
+import de.rougemaster.dungeon.game.gameCommand.devilCommands.devilSpikeShieldCommand;
+import de.rougemaster.dungeon.game.gameCommand.playableCharacterCommands.attackUsingEquipmentCommand;
+import de.rougemaster.dungeon.game.gameCommand.playableCharacterCommands.defendingPlayerCommand;
+import de.rougemaster.dungeon.game.gameCommand.playableCharacterCommands.useItemInFightCommand;
+import de.rougemaster.dungeon.game.gameCommand.skeletonCommands.skeletonBoneAttackCommand;
+import de.rougemaster.dungeon.game.gameCommand.skeletonCommands.skeletonBonesplosionAttackCommand;
+import de.rougemaster.dungeon.game.gameCommand.skeletonCommands.skeletonSwordAttackCommand;
+import de.rougemaster.dungeon.game.gameCommand.zombieCommands.zombieBiteAttackCommand;
+import de.rougemaster.dungeon.game.gameCommand.zombieCommands.zombieClawAttackCommand;
 
 
 import java.util.*;
@@ -40,11 +53,12 @@ public class TurnManager {
      */
     private Map<Character,GameCommand> startTurn(){
         Map<Character,GameCommand> copyCommandMap= commandMap;
-        List<Character> allFights= fightManager.getAllCharactersInFights();
+        List<Character> allCharactersInFights= fightManager.getAllCharactersInFights();
+        setAllFightActions(allCharactersInFights);
         fightManager.executeAllTurns();
         for (Map.Entry<Character, GameCommand> entry : commandMap.entrySet()) {
             Character currentCharacter = entry.getKey();
-            if(allFights.contains(currentCharacter)){
+            if(allCharactersInFights.contains(currentCharacter)){
                 doNothingGameCommand newCommand = new doNothingGameCommand(character);
                 entry.setValue(newCommand);
             }
@@ -52,6 +66,52 @@ public class TurnManager {
 
         return copyCommandMap;
     }
+
+    private void setAllFightActions(List<Character> allFighters){
+        for (Map.Entry<Character, GameCommand> entry : commandMap.entrySet()) {
+            Character currentCharacter = entry.getKey();
+            GameCommand command = entry.getValue();
+            if(allFighters.contains(currentCharacter)){
+                Fight fight = fightManager.getFight(currentCharacter);
+                Character combatantOne = fight.getCombatantOne();
+                Character combatantTwo = fight.getCombatantTwo();
+                if (currentCharacter == combatantOne){
+                    handleFightAction(fight, combatantOne, command);
+                }else{
+                    handleFightAction(fight, combatantTwo, command);
+                }
+            }
+        }
+    }
+
+    private void handleFightAction(Fight fight, Character combatant, GameCommand command) {
+        if (command instanceof doNothingGameCommand) {
+            fight.setCombatantAction(combatant, CombatAction.DO_NOTHING);
+        } else if (command instanceof attackUsingEquipmentCommand) {
+            fight.setCombatantAction(combatant, CombatAction.ATTACK);
+        } else if (command instanceof defendingPlayerCommand) {
+            fight.setCombatantAction(combatant, CombatAction.DEFEND);
+        } else if (command instanceof useItemInFightCommand) {
+            fight.setCombatantAction(combatant, CombatAction.USE_ITEM);
+        } else if (command instanceof skeletonSwordAttackCommand) {
+            fight.setCombatantAction(combatant, CombatAction.SKELETON_SWORD_ATTACK);
+        } else if (command instanceof skeletonBoneAttackCommand) {
+            fight.setCombatantAction(combatant, CombatAction.SKELETON_BONE_ATTACK);
+        } else if (command instanceof skeletonBonesplosionAttackCommand) {
+            fight.setCombatantAction(combatant, CombatAction.SKELETON_BONESPLOSION_ATTACK);
+        } else if (command instanceof zombieClawAttackCommand) {
+            fight.setCombatantAction(combatant, CombatAction.ZOMBIE_CLAW_ATTACK);
+        } else if (command instanceof zombieBiteAttackCommand) {
+            fight.setCombatantAction(combatant, CombatAction.ZOMBIE_BITE_ATTACK);
+        } else if (command instanceof devilFlameSwordAttackCommand) {
+            fight.setCombatantAction(combatant, CombatAction.DEVIL_FLAME_SWORD_ATTACK);
+        } else if (command instanceof devilSpikeShieldCommand) {
+            fight.setCombatantAction(combatant, CombatAction.DEVIL_SPIKE_SHIELD);
+        } else if (command instanceof devilPlayerKillerAttackCommand) {
+            fight.setCombatantAction(combatant, CombatAction.DEVIL_PLAYER_KILLER_ATTACK);
+        }
+    }
+
 
     /**
      * K端mmert sich um den konkreten Input des Caracters. Geht mit den konkreten Commands um, die ein Spieler/Gegener
