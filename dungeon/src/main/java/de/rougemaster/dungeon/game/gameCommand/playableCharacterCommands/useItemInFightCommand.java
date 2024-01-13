1b252dfbb033b9dfaaf28021c6fba090f55f578a
Maurice <maurice.duemmel@haw-hamburg.de>
2024-01-13 19:00:29 +0100

    refactor

--- dungeon/src/main/java/de/rougemaster/dungeon/game/gameCommand/playableCharacterCommands/useItemInFightCommand.java
+++ dungeon/src/main/java/de/rougemaster/dungeon/game/gameCommand/playableCharacterCommands/useItemInFightCommand.java
@@ -2,7 +2,7 @@ package de.rougemaster.dungeon.game.gameCommand.playableCharacterCommands;
 
 import de.rougemaster.dungeon.game.gameCommand.GameCommand;
 
-public class useItemPlayerCommand extends GameCommand {
+public class useItemInFightCommand extends GameCommand {
     @Override
     public void execute() {
 
