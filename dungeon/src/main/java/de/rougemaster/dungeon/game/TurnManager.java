f9b195cc2f894952e2f8db9920434280feb0ef8d
Maurice <maurice.duemmel@haw-hamburg.de>
2024-01-18 19:23:47 +0100

    fix

--- dungeon/src/main/java/de/rougemaster/dungeon/game/TurnManager.java
+++ dungeon/src/main/java/de/rougemaster/dungeon/game/TurnManager.java
@@ -83,7 +83,7 @@ public class TurnManager {
     }
 
     private void setAllFightActions(List<Character> allFighters) {
-        /*for (Map.Entry<Character, GameCommand> entry : commandMap.entrySet()) {
+        for (Map.Entry<Character, GameCommand> entry : commandMap.entrySet()) {
             Character currentCharacter = entry.getKey();
             GameCommand command = entry.getValue();
             if (allFighters.contains(currentCharacter)) {
@@ -96,7 +96,7 @@ public class TurnManager {
                     handleFightAction(fight, combatantTwo, command);
                 }
             }
-        }*/
+        }
     }
 
     private void handleFightAction(Fight fight, Character combatant, GameCommand command) {
